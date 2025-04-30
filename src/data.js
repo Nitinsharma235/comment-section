7f7ea477ac8294903ebafd1ef5e196d5b978f40f
Nitinsharma235 <sharmanit235@gmail.com>
2025-04-30 16:06:08 +0530

    #

---
+++ src/data.js
@@ -0,0 +1,10 @@
+export const dummyComments = [
+    {"comment":"first comment","uuid":1,"author":"a1","upvote":2,"timestamp":"Mon Apr 14 2025 22:46:54 GMT+0530 (India Standard Time)","replies":[]},
+    {"comment":"second comment","uuid":2,"author":"b1","upvote":0,"timestamp":"Mon Apr 17 2025 22:46:54 GMT+0530 (India Standard Time)","replies":[]},
+    {"comment":"third comment","uuid":3,"author":"c1","upvote":8,"timestamp":"Mon Apr 18 2025 22:46:54 GMT+0530 (India Standard Time)","replies":[
+        {"comment":"third r1 comment","uuid":4,"author":"c2","upvote":0,"timestamp":"Mon Apr 19 2025 22:46:54 GMT+0530 (India Standard Time)","replies":[]},
+        {"comment":"third r2 comment","uuid":5,"author":"c3","upvote":4,"timestamp":"Mon Apr 20 2025 22:46:54 GMT+0530 (India Standard Time)","replies":[]}
+    ]},
+    {"comment":"fourth comment","uuid":6,"author":"a2","upvote":3,"timestamp":"Mon Apr 15 2025 22:46:54 GMT+0530 (India Standard Time)","replies":[]},  
+];
+// ⬆️⬇️
